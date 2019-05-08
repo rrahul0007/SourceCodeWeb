b7d2ce7012be48b9c0a8263a47aa4420e214ef89
rrahul0007 <49675677+rrahul0007@users.noreply.github.com>
2019-05-08 20:09:41 +0200

    Global.asax
    Global.asax  file

---
+++ Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Qilin.Web.Core.Models;
+using BlastManagerWEB.Models.Account;
+namespace BlastManagerWEB
+{
+    /// <summary>
+    /// MvcApplication
+    /// </summary>
+    /// <returns></returns>
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Application_Start
+        /// </summary>
+        /// <returns></returns>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        /// <summary>
+        /// Application_BeginRequest
+        /// </summary>
+        /// <returns></returns>
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {      
+            HttpCookie cookie = HttpContext.Current.Request.Cookies["Language"];
+            if (cookie != null && cookie.Value != null)
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cookie.Value);
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(cookie.Value);
+            }
+
+            else
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+            }
+
+
+
+        }
+    }
+}
